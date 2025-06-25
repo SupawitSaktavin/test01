454a1ec1801887616090b81e64f8f51b2bcadb33
SupawitSaktavin <supawit.sa@kkumail.com>
2025-06-25 16:11:05 +0700

    attribute name

--- Student.java
+++ Student.java
@@ -1,6 +1,16 @@
 class Student {
+    private String name;
+
     public Student() {
         //add a dafault constructor of Student
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
     }
   
 }
